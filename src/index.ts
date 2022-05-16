704b32afe65428e8e87f5367ed9b117907d88971
Laimonas Janutenas <laimonas.janutenas@zyro.com>
2022-05-16 10:50:17 +0300

    connecting to postgres

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello world!!');
+import { createConnection } from "typeorm"
+
+const main = async () => {
+    const connection = await createConnection({
+        type: "postgres",
+        host: "localhost",
+        port: 5433,
+        username: "laimonasjanutenas",
+        password: undefined,
+        database: 'typeorm'
+    })
+
+    console.log('aa')
+}
+
+main()
