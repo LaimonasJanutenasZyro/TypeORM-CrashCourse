ce9d3f4532c6ad2243b62182acbee7181a6f4425
Laimonas Janutenas <laimonas.janutenas@zyro.com>
2022-05-16 11:53:09 +0300

    basic types and express init

---
+++ src/entities/Transaction.ts
@@ -0,0 +1,32 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from "typeorm"
+import { Client } from "./Client"
+
+export enum TransactionTypes {
+    DEPOSIT = "DEPOSIT",
+    WITHDRAW = "WITHDRAW"
+}
+
+@Entity("transactions")
+export class Transaction extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({
+        type: "enum",
+        enum: TransactionTypes,
+    })
+    type: string
+
+    @Column({
+        type: "numeric",
+    })
+    amount: number
+
+    @ManyToOne(
+        () => Client,
+        client => client.transactions
+    )
+    @JoinColumn({ name: "client_id" })
+    client: Client
+}
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
 import { createConnection } from "typeorm"
+import { Client } from './entities/Client'
+import { Banker } from './entities/Banker'
+import { Transaction } from "./entities/Transaction"
+import express from "express"
+
+const app = express()
 
 const main = async () => {
-    const connection = await createConnection({
-        type: "postgres",
-        host: "localhost",
-        port: 5433,
-        username: "laimonasjanutenas",
-        password: undefined,
-        database: 'typeorm'
-    })
+    try {
+        const connection = await createConnection({
+            type: "postgres",
+            host: "localhost",
+            port: 5433,
+            username: "laimonasjanutenas",
+            password: undefined,
+            database: 'typeorm',
+            entities: [Client, Banker, Transaction],
+            synchronize: true
+        })
 
-    console.log('aa')
+        console.log('connected to database')
+
+        app.listen(8080, () => {
+            console.log('now running on port 8080')
+        })
+    } catch (err) {
+        console.error(err)
+        throw new Error("unable to connect to database")
+    }
+
+
+
 }
 
 main()
